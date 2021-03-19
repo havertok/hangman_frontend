6b90c5479edfa7a046928d4924657f27d0e16ed2
Elostirion <havertok@gmail.com>
2021-03-19 08:19:25 -0700

    react form to register user now matches DTO in server

--- src/hooks/useSignUpForm.js
+++ src/hooks/useSignUpForm.js
@@ -14,7 +14,8 @@ const useSignUpForm = (callback) => {
     const handleInputChange= (event) => {
         event.persist(); //removes event from event pool; as react otherwise puts all events into a batch
         //event.target will refer to parts of our form; '...' auto spreads iterables
-        setInputs(inputs => ({...inputs, [event.target.name]:[event.target.value]}));
+        //[event.target.value] resulted in sending an object in this form:  {var: [val]} which did not matach up to DTO wouldn't parse
+        setInputs(inputs => ({...inputs, [event.target.name]: event.target.value }));
     }
 
     return {
